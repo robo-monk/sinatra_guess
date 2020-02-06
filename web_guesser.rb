bdde4bb5e749f19a19f73357784ccb21ec6a1a91
robo-monk <filtap007@gmail.com>
2020-02-06 15:29:29 +0100

    uses erb template & html

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,5 +4,6 @@ require 'sinatra/reloader'
 random_num = rand(100)
 
 get '/' do
-  "The SECRET NUMBER is #{random_num}"
+  # "The SECRET NUMBER is #{random_num}"
+  erb :index, :locals => {:num => random_num}
 end
