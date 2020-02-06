59379ec0ab3b3340e739d5787b82ac637fa987ce
robo-monk <filtap007@gmail.com>
2020-02-06 15:55:25 +0100

    added way too low/high msgs

--- web_guesser.rb
+++ web_guesser.rb
@@ -11,10 +11,14 @@ get '/' do
   else
     diff = params['guess'].to_i - random_num
     case diff
-    when 1..Float::INFINITY
+    when 1..5
       erb :index, :locals => {:num => "xx", :msg => 'Too high bro' }
-    when -Float::INFINITY..-1
+    when -5..-1
       erb :index, :locals => {:num => "xx", :msg => 'Too low bro' }
+    when 5..Float::INFINITY
+      erb :index, :locals => {:num => "xx", :msg => 'Way Too high bro' }
+    when -Float::INFINITY..-5
+      erb :index, :locals => {:num => "xx", :msg => 'Way Too low bro' }
     else
       erb :index, :locals => {:num => random_num, :msg => 'bullseye bro' }
     end
