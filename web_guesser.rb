92064c2d49fb3944364262fda89f81d85e54bb9d
robo-monk <filtap007@gmail.com>
2020-02-06 15:53:42 +0100

    added user input eval and message output

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,5 +5,20 @@ random_num = rand(100)
 
 get '/' do
   # "The SECRET NUMBER is #{random_num}"
-  erb :index, :locals => {:num => random_num}
+  # throw params.inspect
+  if params['guess'].nil?
+    erb :index, :locals => {:num => "xx", :msg => 'oof'}
+  else
+    diff = params['guess'].to_i - random_num
+    case diff
+    when 1..Float::INFINITY
+      erb :index, :locals => {:num => "xx", :msg => 'Too high bro' }
+    when -Float::INFINITY..-1
+      erb :index, :locals => {:num => "xx", :msg => 'Too low bro' }
+    else
+      erb :index, :locals => {:num => random_num, :msg => 'bullseye bro' }
+    end
+  end
 end
+
+
